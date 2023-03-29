c484ed9516b2f8e2c10f79eb73c0b783bad3c281
Nesrinemh <nesrine.hadboun@live.fr>
2023-03-29 10:02:08 +0200

    finish last

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ import PropTypes  from "prop-types"
 import Button from "./Button"
 
 const Header = ({title, onAdd, showAdd }) => {
-    const onClick = () => {
-        console.log('object')
-    }
+   
   return (
     <header className="header">
         <h1>{title}</h1>
